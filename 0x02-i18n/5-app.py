b8b18289669c4f69bc0c456a6568d7cb41767ced
chineduCoded <chinedujohn17@gmail.com>
2023-08-02 01:42:37 +0000

    Force locale with URL parameter

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -10,6 +10,14 @@ from flask import (
 from flask_babel import Babel
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
 class Config(object):
     """
     Configuration for Babel
@@ -22,6 +30,26 @@ class Config(object):
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
+
+def get_user():
+    """
+    Return a user dictionary or None if ID can't be found
+    or if 'login_as' URL parameter was not found
+    """
+    id = request.args.get('login_as', None)
+    if id is not None and int(id) in users.keys():
+        return users.get(int(id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    Add user to flask.g is user is found
+    """
+    user = get_user()
+    g.user = user
 
 
 @babel.localeselector
@@ -40,7 +68,7 @@ def index() -> str:
     """
     Handles / route
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
